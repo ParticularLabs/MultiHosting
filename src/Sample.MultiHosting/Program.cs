6a6301ae2e8de4c495864aba2805deb561839777
kentdr <dan.kent@particular.net>
2024-01-23 12:30:18 -0500

    Add multi-hosting sample

---
+++ src/Sample.MultiHosting/Program.cs
@@ -0,0 +1,91 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+Console.Title = "Samples.MultiHosting";
+
+#region multi-hosting-startup
+
+using var endpointOneBuilder = ConfigureEndpointOne(Host.CreateDefaultBuilder(args)).Build();
+using var endpointTwoBuilder = ConfigureEndpointTwo(Host.CreateDefaultBuilder(args)).Build();
+
+await Task.WhenAll(endpointOneBuilder.StartAsync(), endpointTwoBuilder.StartAsync()).ConfigureAwait(true);
+await Task.WhenAll(endpointOneBuilder.WaitForShutdownAsync(), endpointTwoBuilder.WaitForShutdownAsync()).ConfigureAwait(true);
+
+#endregion
+
+static IHostBuilder ConfigureEndpointOne(IHostBuilder builder)
+{
+    builder.UseConsoleLifetime();
+    builder.ConfigureLogging((ctx, logging) =>
+    {
+        logging.AddConfiguration(ctx.Configuration.GetSection("Logging"));
+        logging.AddEventLog();
+        logging.AddConsole();
+    });
+
+    builder.UseNServiceBus(ctx =>
+    {
+        #region multi-hosting-assembly-scan
+
+        var endpointConfiguration = new EndpointConfiguration("Instance1");
+        var scanner = endpointConfiguration.AssemblyScanner();
+        scanner.ExcludeAssemblies("Instance2");
+
+        #endregion
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+
+        return endpointConfiguration;
+    });
+
+    return builder;
+}
+
+static IHostBuilder ConfigureEndpointTwo(IHostBuilder builder)
+{
+    builder.UseConsoleLifetime();
+    builder.ConfigureLogging((ctx, logging) =>
+    {
+        logging.AddConfiguration(ctx.Configuration.GetSection("Logging"));
+        logging.AddEventLog();
+        logging.AddConsole();
+    });
+
+    builder.UseNServiceBus(ctx =>
+    {
+        var endpointConfiguration = new EndpointConfiguration("Instance2");
+        var scanner = endpointConfiguration.AssemblyScanner();
+        scanner.ExcludeAssemblies("Instance1");
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+
+        return endpointConfiguration;
+    });
+
+    return builder;
+}
+
+static async Task OnCriticalError(ICriticalErrorContext context, CancellationToken cancellationToken)
+{
+    var fatalMessage = "The following critical error was " +
+                       $"encountered: {Environment.NewLine}{context.Error}{Environment.NewLine}Process is shutting down. " +
+                       $"StackTrace: {Environment.NewLine}{context.Exception.StackTrace}";
+
+    try
+    {
+        await context.Stop(cancellationToken).ConfigureAwait(false);
+    }
+    finally
+    {
+        Environment.FailFast(fatalMessage, context.Exception);
+    }
+}
+
---
+++ src/Sample.Sender/Program.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+
+var sendOnlyInstance = await StartSendOnlyEndpoint(CancellationToken.None).ConfigureAwait(true);
+
+try
+{
+    Console.WriteLine("Press '1' to send a message from this endpoint to Instance1");
+    Console.WriteLine("Press '2' to send a message from this endpoint to Instance2");
+    Console.WriteLine("Press any key to exit");
+
+    while (true)
+    {
+        var key = Console.ReadKey();
+        Console.WriteLine();
+        var message = new MyMessage();
+        if (key.Key == ConsoleKey.D1)
+        {
+            await sendOnlyInstance.Send("Instance1", message)
+                .ConfigureAwait(false);
+            continue;
+        }
+        if (key.Key == ConsoleKey.D2)
+        {
+            await sendOnlyInstance.Send("Instance2", message)
+                .ConfigureAwait(false);
+            continue;
+        }
+        return;
+    }
+}
+finally
+{
+    if (sendOnlyInstance != null)
+    {
+        await sendOnlyInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
+
+
+static Task<IEndpointInstance> StartSendOnlyEndpoint(CancellationToken cancellationToken)
+{
+    var endpointConfiguration = new EndpointConfiguration("Samples.MultiHosting.SendOnly");
+    _ = endpointConfiguration.AssemblyScanner();
+
+    _ = endpointConfiguration.UsePersistence<LearningPersistence>();
+    _ = endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+    _ = endpointConfiguration.UseTransport(new LearningTransport());
+
+    return Endpoint.Start(endpointConfiguration, cancellationToken);
+}
+
